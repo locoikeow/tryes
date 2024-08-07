ca350ae68595cd0180b42be559f06b1d512e5a22
George <159633035+iGoerge@users.noreply.github.com>
2024-08-07 12:48:58 +0200

    dsa

---
+++ datos-simples.py
@@ -0,0 +1,22 @@
+# texto (no permiten saltos de linea)
+"string"
+'string'
+
+# texto (permiten saltos de linea)
+"""tus datos son:
+    nombre: lucas
+    apellido: dalto"""
+
+'''tus datos son:
+    nombre: lucas
+    apellido: dalto'''
+
+# int(numeros esteros)
+40
+
+# flot(con coma '.')
+40.2
+
+#Bool (Si o No)
+True
+False
